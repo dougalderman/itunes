856f668a11fe623cf7f2580aed3325e6c98f08b7
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-22 17:36:37 -0700

    12/22/15 changes

--- js/itunesService.js
+++ js/itunesService.js
@@ -6,9 +6,30 @@ app.service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+
+    this.getData = function(artistName) {
+
+      var deferred = $q.defer();
+
+      var url = 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK';
+      $http.get(url)
+      .then(function(response) {
+          console.log(response);
+          if (response.data.data) {
+            var parsedArray = response.data.data;
+          } else {
+            var parsedArray
+          }
+        })
+        .catch(function(err) {
+          deferred.reject(err);
+        })
+        return deferred.promise;
+
+    }
+
 });
